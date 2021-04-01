3ab4353595c87dba587c0c88bfb07396b47a8c15
toshiba <@>
2021-04-01 14:57:32 -0400

    Add WriteConfigDump() in config.php

--- default/template/php/config.php
+++ default/template/php/config.php
@@ -3,55 +3,66 @@
 
 include_once('utils.php');
 
-function index ($string, $needle) { // emulates perl's index(), returning -1 instead of false
-	$strpos = strpos($string, $needle);
-	if ($strpos === false) {
-		return -1;
-	} else {
-		return $strpos;
+//
+//echo(time());
+//
+//echo "<br><hr><br>";
+//echo "<br>Below is set of local config which differs from defaults<br>";
+//echo "<br>You can copy and paste this into Write form of your own site, sign it, and post as Operator.";
+//echo "<br><hr><br>";
+
+function WriteConfigDump() {
+	#todo use GetDir()
+	$default = explode("\n", `find ../default`);
+	$config = explode("\n", `find ../config`);
+
+	$text = "";
+	$text = time() . " #config #example\n\n";
+	$configLookup = array();
+
+	foreach ($config as $c) {
+		$c = str_replace('../config/', '', $c);
+		$configLookup[$c] = 1;
+		#todo avoid checking directories with file_exists(), causes warning
+		$configValue[$c] = file_exists('../config/' . $c) ? trim(file_get_contents('../config/' . $c)) : '';
+		$defaultValue[$c] = file_exists('../default/' . $c) ? trim(file_get_contents('../default/' . $c)) : '';
 	}
-}
 
-echo(time());
+	foreach ($default as $d) {
+		if (index($d, 'secret') == -1) {
+			$d = str_replace('../default/', '', $d);
 
-echo "<br><hr><br>";
-echo "<br>Below is set of local config which differs from defaults<br>";
-echo "<br>You can copy and paste this into Write form of your own site, sign it, and post as Operator.";
-echo "<br><hr><br>";
+	//		print (isset($configLookup[$d]) ? $configLookup[$d] : '');
 
-$default = explode("\n", `find ../default`);
-$config = explode("\n", `find ../config`);
+			if (isset($configLookup[$d])) {
+				//print ('<b>+</b>');
+				if ($configValue[$d] == $defaultValue[$d]) {
+					//print 'default';
+				} else {
+					$text .= "config/";
+					$text .=  $d;
+					$text .=  '=';
+					$text .=  $configValue[$d];
 
-$configLookup = array();
-
-foreach ($config as $c) {
-	$c = str_replace('../config/', '', $c);
-	$configLookup[$c] = 1;
-
-	$configValue[$c] = file_exists('../config/' . $c) ? trim(file_get_contents('../config/' . $c)) : '';
-	$defaultValue[$c] = file_exists('../default/' . $c) ? trim(file_get_contents('../default/' . $c)) : '';
-}
-
-foreach ($default as $d) {
-	if (index($d, 'secret') != -1) {
-		$d = str_replace('../default/', '', $d);
-
-//		print (isset($configLookup[$d]) ? $configLookup[$d] : '');
-
-		if (isset($configLookup[$d])) {
-			//print ('<b>+</b>');
-			if ($configValue[$d] == $defaultValue[$d]) {
-				//print 'default';
-			} else {
-				print "config ";
-				print $d;
-				print ' ';
-				print htmlspecialchars(trim($configValue[$d]));
-				print "<br>";
+					//print htmlspecialchars(trim($configValue[$d]));
+					//print "<br>";
+					$text .= "\n";
+				}
 			}
-		}
 
+		}
 	}
-}
+
+	#todo use GetDir()
+	$targetFileName = "./txt/config_dump_" . time() . ".txt";
+
+	WriteLog($text);
+	WriteLog($targetFileName);
+
+	PutFile($targetFileName, $text);
+	file_put_contents($targetFileName, $text);
+
+	return $targetFileName;
+} # DoConfigDump()
 
 /* / php/config.php */
