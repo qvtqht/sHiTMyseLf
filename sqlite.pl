2a013d66984546bdca7cb5e674a5f06c8b47656e
toshiba <@>
2021-06-04 14:50:30 -0400

    sqlite.pl: format query

--- sqlite.pl
+++ sqlite.pl
@@ -499,13 +499,13 @@ sub SqliteMakeTables { # creates sqlite schema
 				CAST(item_order.item_order AS INTEGER) AS item_order
 			FROM
 				item
-				LEFT JOIN child_count ON ( item.file_hash = child_count.parent_hash)
-				LEFT JOIN parent_count ON ( item.file_hash = parent_count.item_hash)
-				LEFT JOIN added_time ON ( item.file_hash = added_time.file_hash)
-				LEFT JOIN item_title ON ( item.file_hash = item_title.file_hash)
-				LEFT JOIN item_name ON ( item.file_hash = item_name.file_hash)
-				LEFT JOIN item_order ON ( item.file_hash = item_order.file_hash)
-				LEFT JOIN item_author ON ( item.file_hash = item_author.file_hash)
+				LEFT JOIN child_count ON ( item.file_hash = child_count.parent_hash )
+				LEFT JOIN parent_count ON ( item.file_hash = parent_count.item_hash )
+				LEFT JOIN added_time ON ( item.file_hash = added_time.file_hash )
+				LEFT JOIN item_title ON ( item.file_hash = item_title.file_hash )
+				LEFT JOIN item_name ON ( item.file_hash = item_name.file_hash )
+				LEFT JOIN item_order ON ( item.file_hash = item_order.file_hash )
+				LEFT JOIN item_author ON ( item.file_hash = item_author.file_hash )
 				LEFT JOIN item_score ON ( item.file_hash = item_score.file_hash)
 				LEFT JOIN item_tags_list ON ( item.file_hash = item_tags_list.file_hash )
 				LEFT JOIN item_sequence ON ( item.file_hash = item_sequence.file_hash )
