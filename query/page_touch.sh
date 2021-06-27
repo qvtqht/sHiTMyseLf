d44d99181b0cf319019b4f095a667bba08056eb0
toshiba <@>
2021-06-26 22:54:08 -0400

    slightly improve query shellscripts

--- query/page_touch.sh
+++ query/page_touch.sh
@@ -5,5 +5,5 @@
 
 # this script assumes existence of one cache/*/index.sqlite
 
-sqlite3 -echo -cmd ".headers on" -cmd ".timeout 500" -cmd ".mode column" cache/*/index.sqlite3 "SELECT priority, COUNT(*) AS count FROM task GROUP BY priority; SELECT * FROM task ORDER BY priority DESC, touch_time DESC LIMIT 25"
+sqlite3 -echo -cmd ".headers on" -cmd ".timeout 500" -cmd ".mode column" cache/*/index.sqlite3 "SELECT priority, COUNT(*) AS count FROM task GROUP BY priority ORDER BY priority; SELECT * FROM task ORDER BY priority DESC, touch_time DESC LIMIT 25"
 
--- query/page_touch_all.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-sqlite3 -echo -cmd ".headers on" -cmd ".timeout 500" -cmd ".mode column" cache/*/index.sqlite3 'update task set priority=priority+1'
