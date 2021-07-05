0d34e43529ed275e9165fb50817e6cb0f69612d2
toshiba <@>
2021-07-05 15:43:22 -0400

    improve _dev_float_meta.sh

--- _dev_float_meta.sh
+++ _dev_float_meta.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 
-echo Combing archives for 'meta', writing import script...
+needle=meta
+echo $needle
 
-echo \#!/bin/sh > ./_temp_import_meta_items_from_archives.sh
+echo 'Combing archives for "' $needle '" and writing import script...'
+
+echo \#!/bin/sh > ./_temp_import_needle_items_from_archives.sh
 
 for f in ./archive/*.gz; do
-  echo Searching in $f for 'meta'
-  tar -xzf $f --to-command='grep -iHnl --label="tar -zxvf $TAR_ARCHIVE $TAR_FILENAME ; mv $TAR_FILENAME html/txt" meta || true' | grep "txt ;" >> ./_temp_import_meta_items_from_archives.sh
-  echo Found: `wc -l _temp_import_meta_items_from_archives.sh`
+  echo 'Searching in' $f 'for "' $needle '"'
+  tar -xzf $f --to-command='grep -iHnl --label="tar -zxvf $TAR_ARCHIVE $TAR_FILENAME ; mv $TAR_FILENAME html/txt" meta || true' | grep "txt ;" >> ./_temp_import_needle_items_from_archives.sh
+  echo Found: `wc -l _temp_import_needle_items_from_archives.sh`
 done
 
 chmod +x ./_temp_import_meta_items_from_archives.sh
