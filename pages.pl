9f6c6bd5fa140b02022a68741dad2d946be5e56f
toshiba <@>
2021-07-05 14:55:20 -0400

    add GetAccessDialog()

--- pages.pl
+++ pages.pl
@@ -5017,6 +5017,22 @@ sub GetOperatorWindow {
 	#return 'hi';
 }
 
+sub GetAccessDialog {
+	my $accessTemplate = GetTemplate('html/access.template');
+	if (GetConfig('admin/js/enable')) {
+		$accessTemplate = AddAttributeToTag($accessTemplate, 'input value="Beginner"', 'onclick', "if (window.SetInterfaceMode) { return SetInterfaceMode('beginner', this); }");
+		$accessTemplate = AddAttributeToTag($accessTemplate, 'input value="Intermediate"', 'onclick', "if (window.SetInterfaceMode) { return SetInterfaceMode('intermediate', this); }");
+		$accessTemplate = AddAttributeToTag($accessTemplate, 'input value="Advanced"', 'onclick', "if (window.SetInterfaceMode) { return SetInterfaceMode('expert', this); }");
+	}
+
+	$accessTemplate = GetWindowTemplate(
+		$accessTemplate,
+		'Interface'
+	);
+	$accessTemplate =~ s/access\.html/settings.html/;
+	return $accessTemplate;
+} # GetAccessDialog()
+
 sub GetSettingsPage { # returns html for settings page (/settings.html)
 	my $txtIndex = "";
 
